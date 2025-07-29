21aa034efff7892bf821ed512a185aee61ef8e0c
Edison-Paulino <EDPJ0001@ce.pucmm.edu.do>
2025-07-28 23:45:22 -0400

    intento de mejora graficos

--- paginajammer/monitor/JammerP2_NoGui.py
+++ paginajammer/monitor/JammerP2_NoGui.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from gnuradio import gr, blocks, analog, fft
+from gnuradio import gr, blocks, analog
+from gnuradio.fft import window, logpwrfft
 from gnuradio.filter import firdes
-from gnuradio.fft import window
 import configparser
 import threading
 import time
@@ -12,32 +9,23 @@ import signal
 import sys
 import numpy as np
 from gnuradio import zeromq
+import logging
 
 CONFIG_PATH = r"D:\django_project\paginajammer\paginajammer\CONFIG.INI"
 
-
-import logging
-import sys
-
 log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-
 file_handler = logging.FileHandler("jammer_debug.log")
 file_handler.setFormatter(log_formatter)
-
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setFormatter(log_formatter)
 
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(file_handler)
 logging.getLogger().addHandler(console_handler)
-
 log = logging.info
+
 log(" Logging iniciado correctamente")
-
-log = logging.info
 log(" Script JammerP2_NoGui iniciado correctamente")
-
-
 
 class JammerNoGUI(gr.top_block):
     def __init__(self):
@@ -49,7 +37,7 @@ class JammerNoGUI(gr.top_block):
 
         # Variables
         self.vector_size = 8192
-        self.sample_rate_rx = 160e3
+        self.sample_rate_rx = 2e6  # Mejor resolución espectral
         self.sample_rate_tx = self.Samp_Rate_TX
         self.freq = self.frec
         self.bandwidth = self.bandwidth
@@ -65,10 +53,10 @@ class JammerNoGUI(gr.top_block):
 
         self.src.set_sample_rate(self.sample_rate_rx)
         self.src.set_center_freq(self.freq)
-        self.src.set_gain(73, 0)
-        self.src.set_if_gain(73, 0)
-        self.src.set_bb_gain(0, 0)
-        self.src.set_bandwidth(28e6, 0)
+        self.src.set_gain(self.rf_gain, 0)
+        self.src.set_if_gain(self.if_gain, 0)
+        self.src.set_bb_gain(self.bb_gain, 0)
+        self.src.set_bandwidth(self.bandwidth, 0)
 
         # SDR Sumidero (transmisión)
         self.sink = osmosdr.sink(args="numchan=1")
@@ -82,8 +70,8 @@ class JammerNoGUI(gr.top_block):
         # Generadores de ruido + tonos
         self.tone1 = analog.sig_source_c(20e6, analog.GR_COS_WAVE, self.cosine_freq, 1, 0)
         self.tone2 = analog.sig_source_c(20e6, analog.GR_COS_WAVE, self.cosine_freq, 1, 0.5)
-        self.noise1 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 1, 0, 8192)
-        self.noise2 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 1, 165, 8192)
+        self.noise1 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 1, 0, self.vector_size)
+        self.noise2 = analog.fastnoise_source_c(analog.GR_GAUSSIAN, 1, 165, self.vector_size)
 
         self.mult1 = blocks.multiply_vcc(1)
         self.mult2 = blocks.multiply_vcc(1)
@@ -93,13 +81,19 @@ class JammerNoGUI(gr.top_block):
         self.selector_block.set_enabled(True)
 
         self.null = blocks.null_source(gr.sizeof_gr_complex)
-        
-        # FFT para análisis
-        self.stream_to_vector = blocks.stream_to_vector(gr.sizeof_gr_complex, self.vector_size)
-        self.fft_block = fft.fft_vcc(self.vector_size, True, window.blackmanharris(self.vector_size), True)
-        self.mag_squared = blocks.complex_to_mag_squared(self.vector_size)
-        self.nlog10 = blocks.nlog10_ff(5, self.vector_size, 0)
-        
+
+        # logpwrfft para análisis
+        self.logpwrfft = logpwrfft.logpwrfft_c(
+            sample_rate=self.sample_rate_rx,  
+            fft_size=8192,
+            ref_scale=2,
+            frame_rate=30,
+            avg_alpha=0.5,  # ← prueba este
+            average=True,
+            shift=False
+        )
+
+
         # ZMQ para graficar en web
         self.zeromq_pub = zeromq.pub_sink(gr.sizeof_float, self.vector_size, "tcp://*:5678", 100, False, -1, '', True, True)
 
@@ -110,16 +104,12 @@ class JammerNoGUI(gr.top_block):
         self.connect(self.tone2, (self.mult2, 1))
         self.connect(self.mult1, (self.add, 0))
         self.connect(self.mult2, (self.add, 1))
-
         self.connect(self.add, (self.selector_block, 0))
         self.connect(self.null, (self.selector_block, 1))
         self.connect(self.selector_block, self.sink)
 
-        self.connect(self.src, self.stream_to_vector)
-        self.connect(self.stream_to_vector, self.fft_block)
-        self.connect(self.fft_block, self.mag_squared)
-        self.connect(self.mag_squared, self.nlog10)
-        self.connect(self.nlog10, self.zeromq_pub)
+        self.connect(self.src, self.logpwrfft)
+        self.connect(self.logpwrfft, self.zeromq_pub)
 
         # Lanzar hilo para leer el .ini periódicamente
         self.monitor_thread = threading.Thread(target=self.config_monitor_loop, daemon=True)
--- usuarios/consumers.py
+++ usuarios/consumers.py
@@ -9,15 +9,27 @@ class MonitorConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
         self.running = True
+
+        # Leer CONFIG.INI una vez al inicio
+        self.center_freq = self.get_center_freq()
+        self.samp_rate = 28e6  # Constante
+
+        # Inicializar ZMQ
         self.zmq_context = zmq.asyncio.Context()
         self.zmq_socket = self.zmq_context.socket(zmq.SUB)
         self.zmq_socket.connect("tcp://127.0.0.1:5678")
         self.zmq_socket.setsockopt(zmq.SUBSCRIBE, b"")
+
+        # Lanzar tarea de escucha
         self.fft_task = asyncio.create_task(self.receive_fft())
+
+        # Tarea secundaria para refrescar frecuencia cada 5s
+        self.config_task = asyncio.create_task(self.config_monitor_loop())
 
     async def disconnect(self, close_code):
         self.running = False
         self.fft_task.cancel()
+        self.config_task.cancel()
         try:
             self.zmq_socket.close()
             self.zmq_context.term()
@@ -32,42 +44,41 @@ class MonitorConsumer(AsyncWebsocketConsumer):
         except:
             pass
 
-    async def receive_fft(self):
-        samp_rate = 28e6
-        fft_size = 8192
+    async def config_monitor_loop(self):
         while self.running:
             try:
-                # Leer frecuencia desde .ini
-                config = configparser.ConfigParser()
-                config.read('D:/django_project/paginajammer/paginajammer/CONFIG.INI')
-                center_freq = float(config['PARAMETROS']['frecuencia'])
+                self.center_freq = self.get_center_freq()
+            except Exception as e:
+                print("Error leyendo CONFIG.INI:", e)
+            await asyncio.sleep(5)
 
-                if self.zmq_socket.poll(timeout=1000):  # 1 segundo
+    def get_center_freq(self):
+        config = configparser.ConfigParser()
+        config.read('D:/django_project/paginajammer/paginajammer/CONFIG.INI')
+        return float(config['PARAMETROS']['frecuencia'])
+
+    # 🔄 MODIFICAR receive_fft PARA OMITIR OFFSET Y USAR TODO EL VECTOR COMPLETO
+    async def receive_fft(self):
+        fft_size = 8192
+
+        while self.running:
+            try:
+                if self.zmq_socket.poll(timeout=1000):
                     msg = await self.zmq_socket.recv()
                 else:
                     continue
 
                 raw_data = list(memoryview(msg).cast('f'))
 
-                # Recortar para asegurar tamaño válido
-                data = raw_data[:fft_size]
-
-                #print("Tamaño de data:", len(data))
-                #print("Datos enviados:", data[:10])
-
-
-                if self.scope.get("client") is None:
-                    break
-
-                # Verifica que aún el WebSocket esté abierto
-                if not self.running:
-                    break
+                if len(raw_data) != fft_size:
+                    print("⚠️ Tamaño inesperado de FFT:", len(raw_data))
+                    continue
 
                 await self.send(json.dumps({
                     "type": "fft",
-                    "values": data,
-                    "center_freq": center_freq,
-                    "samp_rate": samp_rate,
+                    "values": raw_data,
+                    "center_freq": self.center_freq,
+                    "samp_rate": self.samp_rate,
                     "fft_size": fft_size
                 }))
 
@@ -78,4 +89,3 @@ class MonitorConsumer(AsyncWebsocketConsumer):
                 print("ZMQ error:", e)
                 await self.send(json.dumps({"type": "error", "detail": str(e)}))
                 await asyncio.sleep(1)
-
